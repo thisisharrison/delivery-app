90bb1b8d2fb3dc8efdf70e794bdbf1d3818e3da2
thisisharrison <harrison.lau@outlook.com>
2021-05-08 15:28:20 +0800

    Remove unofficial library

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,24 +1,38 @@
-import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
+import React, { useEffect, useRef } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 import PropTypes from "prop-types";
 
-export default class Map extends Component {
-  static defaultProps = {
-    center: { lat: 22.32, lng: 114.17 },
+import "./map.scss";
+
+const Map = (props) => {
+  const loader = new Loader({
+    apiKey: process.env.REACT_APP_MAPS_API_KEY,
+    version: "weekly",
+    libraries: ["places"],
+  });
+
+  const mapOptions = {
+    center: {
+      lat: 22.32,
+      lng: 114.17,
+    },
     zoom: 11,
   };
 
-  render() {
-    return (
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        ></GoogleMapReact>
-      </div>
-    );
-  }
-}
+  const map = useRef(null);
+
+  useEffect(() => {
+    loader
+      .load()
+      .then(() => new window.google.maps.Map(map.current, mapOptions))
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
+
+  return <div id="map" ref={map}></div>;
+};
 
 Map.propTypes = {};
+
+export default Map;
