626cdf43e48922827c330d8f7fc6e04f980ff228
thisisharrison <harrison.lau@outlook.com>
2021-05-10 10:24:11 +0800

    Update index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.scss";
-import * as API from "./util/server_api_util";
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/util/server_api_util.js
+++ src/util/server_api_util.js
@@ -17,60 +17,23 @@ export const getRoute = (token) => {
 export const testServerError = () => {
   return axios.post(apiUrl + "/mock/route/500");
 };
-/**
- * @returns
- * HTTP/1.1 500 Internal Server Error
- * Internal Server Error
- */
 
 // POST /mock/route/success
 export const testSubmitSuccess = () => {
   return axios.post(apiUrl + "/mock/route/success");
 };
-/**
- * @returns
- * HTTP/1.1 200 OK
- * { "token": "9d3503e0-7236-4e47-a62f-8b01b5646c16" }
- */
 
 // GET /mock/route/inprogress
 export const testGetRouteInProgress = () => {
   return axios.get(apiUrl + "/mock/route/inprogress");
 };
-/**
- * @returns
- * HTTP/1.1 200 OK
- * { "status": "in progress" }
- */
 
 // GET /mock/route/failure
 export const testGetRouteFailure = () => {
   return axios.get(apiUrl + "/mock/route/failure");
 };
-/**
- * @returns
- * HTTP/1.1 200 OK
- * {
- *   "status": "failure",
- *   "error": "Location not accessible by car"
- * }
- */
 
 // GET /mock/route/success
 export const testGetRouteSuccess = () => {
   return axios.get(apiUrl + "/mock/route/success");
 };
-/**
- * @returns
- * HTTP/1.1 200 OK
- * {
- *  "status": "success",
- *  "path": [
- *    ["22.372081", "114.107877"],
- *    ["22.326442", "114.167811"],
- *    ["22.284419", "114.159510"]
- *  ],
- *  "total_distance": 20000,
- *  "total_time": 1800
- * }
- */
